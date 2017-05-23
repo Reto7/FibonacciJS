a419844e401c15ed23633c1334d80d6c5762f47d
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-23 21:09:30 +0200

    fibonnaci uebung mit unit-test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,7 @@
 'use strict'
 
-import assert from 'assert'
-
-import fibonacci from '../fibonacci'
+import assert from 'assert'           // js liest aus node_modules
+import fibonacci from '../fibonacci'  // js liest aus unserem verzeichnis
 
 describe('fibonacci test', function () {
      describe('check array entry count', function () {
@@ -24,5 +23,12 @@ describe('fibonacci test', function () {
             let fibonacciArray = fibonacci(10)
             assert.equal(fibonacciArray[9],34)         //[ 0, 1, 1, 2, 3, 5 ]
         })
+    })
+    describe('check array at position negative test', function () {
+        it('should give 2 (position #4)', function () {
+            let fibonacciArray = fibonacci(2)
+            assert.equal(fibonacciArray[13],undefined)
+        })
+
     })
  })
